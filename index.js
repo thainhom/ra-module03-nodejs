9d3f92655719c60452c1d7dcb7b9aa2a87f9f945
ThaiLQ5 <thainhom1206@gmail.com>
2023-07-20 22:28:11 +0700

    session03-bai-tạp-Đọc ghi-cosole các file ra màng hình

--- index.js
+++ index.js
@@ -1,23 +1,91 @@
-const http = require('http');
-const server = http.createServer((request, response) => {
-    const { patname, query } = url.parse(request.url, true);
-    response.writeHead(200, {
-        'Content-Type': 'text/html; charset=UTF-8',
-    });
-    let html = '';
-    html = `
-    <form action=127.0.0.1:8080 method= 'GET' >
-    <input type= 'text' name= keyword />
-    <input type= 'text' name= course />
-    <button type= 'submit'>Tìm kiếm</button>
-    </form>
-    <p> Tu Khoa:${query.keyword}</p>
-    <p>Khoa hoc:${query.course}</p>
-    `
-    response.write(html);
-    response.end();
+const fs = require("fs")
+const students = [
+    {
+        name: 'Trung',
+        age: 25,
+        homeTowm: "Quảng Bình"
+    },
+    {
+        name: 'Tín',
+        age: 29,
+        homeTowm: "Quảng Nam"
+    },
 
-})
-server.listen(8080, '127.0.0.1', () => {
-    console.log("server đang chạy trên cổng 8000, vui lòng try cập http://127.0.0.1:8080/ ");
-})  
+    {
+        name: 'Sơn',
+        age: 25,
+        homeTowm: "Đà Nẵng"
+    },
+
+    {
+        name: 'Giang',
+        age: 21,
+        homeTowm: "Quảng Bình"
+    },
+
+    {
+        name: 'Tài',
+        age: 21,
+        homeTowm: "Quảng Nam"
+    },
+
+    {
+        name: 'Sáng',
+        age: 20,
+        homeTowm: "Hà Tĩnh"
+    },
+
+    {
+        name: 'Việt',
+        age: 21,
+        homeTowm: "Huế"
+    },
+    {
+        name: 'Giang',
+        age: 20,
+        homeTowm: "Huế"
+    },
+
+
+
+
+
+]
+// chạy vong lập for of đê ghi thông tin các sinh viên vào các file riêng biệt
+for (let student of students) {
+    const content = "Tên :" + student.name + '\n' + "Tuổi :" + student.age + '\n' + 'Quê Quán:' + student.homeTowm
+    const fileName = student.name + '.txt'
+    fs.writeFileSync(fileName, content, "utf-8")
+}
+// Đọc và in thông tin từng file sinh viên ra console.log
+for (let student of students) {
+    const fileName = student.name + '.txt'
+    // đọc nội dung từ các file định dạng utf-8
+    try {
+        const data = fs.readFileSync(fileName, "utf-8")
+    } catch (error) {
+        console.error("error reading file ", error)
+    }
+
+}
+// Gom các sinh viên theo quê quán và in ra console.log
+const studentsByhomeTowm = {}
+for (let student of students) {
+    const content = "Tên :" + student.name + '\n' + 'Tuổi:' + student.age + '\n'
+
+    if (studentsByhomeTowm[student.homeTowm]) {
+        // nếu quê quán đã tồn tại trong studentByhomeTowm , thêm thông tin tên và tuổi vào nhóm tương ứng
+        studentsByhomeTowm[student.homeTowm].push(content)
+    } else {
+        // nếu quê quán sinh viên chưa tồn tại tạo một nhóm mới in thông tin sính viên vào 
+        studentsByhomeTowm[student.homeTowm] = [content];
+    }
+}
+// in thông tin gom nhóm ra mà hình cosole.log
+for (const hometown in studentsByhomeTowm) {
+    console.log("Sinh viên từ " + hometown + ":");
+    for (const studentInfo of studentsByhomeTowm[hometown]) {
+        console.log(studentInfo);
+    }
+    console.log("-------------");
+}
