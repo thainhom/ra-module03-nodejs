e42fa00cd725f7f5f4b6700806d03e1f29aa5ef4
ThaiLQ5 <thainhom1206@gmail.com>
2023-07-24 21:17:48 +0700

    session05 CRUD Users

--- src/index.js
+++ src/index.js
@@ -1,46 +1,147 @@
 // const express = require('express');
-import express from 'express';// phai thêm type "module"
+import express from 'express'; // Phải thêm "type": "module" ở package.json
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import fs from 'fs';
-const managaUsers = express();
-// managaUsers.use(express.json())
-// managaUsers.use(express.urlencoded());
-// cấu hình bodyParser
+
+const application = express();
+
+// Cấu hình body parser
 // parse application/x-www-form-urlencoded
-managaUsers.use(bodyParser.urlencoded({ extended: false }))
+application.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-managaUsers.use(bodyParser.json())
-// cấu hình morgan
+application.use(bodyParser.json());
+
+// Cấu hình morgan
 const accessLogStream = fs.createWriteStream('src/logs/access.log', { flags: 'a' });
-managaUsers.use(morgan('combined', { stream: accessLogStream }))
+application.use(morgan('combined', { stream: accessLogStream }));
 
-managaUsers.get("/users", (req, res) => {
-
-    res.send({
-        users: []
-    })
-})
-managaUsers.get('/users/:id', (req, res, next) => {
-
-    if (req.params.id === '0') {
-        next('route')
-    } else {
-        next()
+const getNextId = (items) => {
+    // Trường hợp 1: nếu items rỗng thì next ID sẽ là 1
+    if (items.length === 0) {
+        return 1;
     }
-}, (req, res) => {
+    // Trường hợp 2: Nếu items không rỗng thì next ID sẽ bằng ID lớn nhất trong danh sách items + 1
+    else {
+        // Lấy tất cả ID trong danh sách items lưu vào mảng idList
+        const idList = items.map(todo => {
+            return todo.id;
+        });
 
-    res.send({
-        id: req.params.id
-    })
-})
-managaUsers.post("/users", (req, res) => {
+        // Lấy giá trị ID lớn nhất trong mảng idList
+        const maxId = Math.max(...idList);
 
-    res.send({
-        req: req.body
-    })
+        // Trả về next ID: ID lớn nhất trong danh sách items + 1
+        return maxId + 1;
+    }
+}
+
+let users = []
+
+// Search user
+application.get('/users', (req, res) => {
+    const keyword = req.query.keyword;
+
+    if (keyword !== undefined) {
+        const searchUsers = users.filter(user => {
+            return user.username.toLowerCase().includes(keyword.toLowerCase())
+                || user.email.toLowerCase().includes(keyword.toLowerCase())
+                || user.first_name.toLowerCase().includes(keyword.toLowerCase())
+                || user.last_name.toLowerCase().includes(keyword.toLowerCase())
+        });
+        res.send(searchUsers);
+    } else {
+        res.send(users);
+    }
 })
-managaUsers.listen(8000, () => {
-    console.log('server started');
-})
+
+// Tạo user
+application.post('/users', (req, res) => {
+    users.push({
+        ...req.body,
+        id: getNextId(users),
+        created_at: new Date(),
+        updated_at: new Date()
+    });
+
+    res.status(201) // HTTP status code 201: CREATED
+        .send(req.body);
+});
+
+// Lấy thông tin 1 user
+application.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    const user = users.find(user => user.id == id);
+
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404)
+            .send({
+                error: 'User not found'
+            });
+    }
+});
+
+application.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    // Kiểm tra user với param id có tồn tại không
+    const user = users.find(user => user.id == id);
+
+    // Nếu không tồn tại thì trả về lỗi
+    if (!user) {
+        res.status(404)
+            .send({
+                error: 'User not found'
+            });
+    }
+
+    // Lấy request body
+    const requestBody = req.body;
+    let updatedUser = null;
+
+    users = users.map(user => {
+        if (user.id == id) {
+            updatedUser = {
+                ...user,
+                first_name: requestBody.first_name,
+                last_name: requestBody.last_name,
+                password: requestBody.password ? requestBody.password : user.password,
+                role: requestBody.role,
+                updated_at: new Date()
+            }
+            return updatedUser;
+        } else {
+            return user;
+        }
+    });
+
+    // Lấy lại user sau khi update
+    // const userUpdated = users.find(user => user.id == id);
+    res.send(updatedUser)
+});
+
+application.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    // Kiểm tra user với param id có tồn tại không
+    const user = users.find(user => user.id == id);
+
+    // Nếu không tồn tại thì trả về lỗi
+    if (!user) {
+        res.status(404)
+            .send({
+                error: 'User not found'
+            });
+    }
+
+    users = users.filter(user => user.id != id);
+    res.status(204).send(); // HTTP status code 204 (No Content) - thường được sử dụng để trả về sau khi xóa thành công
+});
+
+application.listen(8000, () => {
+    console.log('Server started');
+});
