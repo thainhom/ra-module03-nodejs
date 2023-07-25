07280585644710df8e09898b62a372e468799f35
ThaiLQ5 <thainhom1206@gmail.com>
2023-07-25 21:06:54 +0700

    session05-bai-tap-products

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const application = express();
 // Cấu hình body parser
 // parse application/x-www-form-urlencoded
 application.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 application.use(bodyParser.json());
 
@@ -37,30 +36,30 @@ const getNextId = (items) => {
     }
 }
 
-let users = []
+let products = [];
 
-// Search user
-application.get('/users', (req, res) => {
+// Search prodducts
+application.get('/products', (req, res) => {
     const keyword = req.query.keyword;
 
     if (keyword !== undefined) {
-        const searchUsers = users.filter(user => {
-            return user.username.toLowerCase().includes(keyword.toLowerCase())
-                || user.email.toLowerCase().includes(keyword.toLowerCase())
-                || user.first_name.toLowerCase().includes(keyword.toLowerCase())
-                || user.last_name.toLowerCase().includes(keyword.toLowerCase())
+        const searchProduct = products.filter(product => {
+            return product.name.toLowerCase().includes(keyword.toLowerCase())
+                || product.price.toLowerCase().includes(keyword.toLowerCase())
+                || product.color.toLowerCase().includes(keyword.toLowerCase())
+
         });
-        res.send(searchUsers);
+        res.send(searchProduct);
     } else {
-        res.send(users);
+        res.send(products);
     }
 })
 
-// Tạo user
-application.post('/users', (req, res) => {
-    users.push({
+// Tạo Products
+application.post('/products', (req, res) => {
+    products.push({
         ...req.body,
-        id: getNextId(users),
+        id: getNextId(products),
         created_at: new Date(),
         updated_at: new Date()
     });
@@ -68,79 +67,122 @@ application.post('/users', (req, res) => {
     res.status(201) // HTTP status code 201: CREATED
         .send(req.body);
 });
-
-// Lấy thông tin 1 user
-application.get('/users/:id', (req, res) => {
+// lấy thông tin một sản phẩm 
+application.get('/products/:id', (req, res) => {
     const { id } = req.params;
-
-    const user = users.find(user => user.id == id);
-
-    if (user) {
-        res.send(user);
+    const product = products.find(product => product.id == id)
+    if (product) {
+        res.send(product);
     } else {
-        res.status(404)
-            .send({
-                error: 'User not found'
-            });
+        res.status(404).send({
+            error: "Sản phẩm không tồn tại "
+        })
     }
-});
-
-application.put('/users/:id', (req, res) => {
+})
+// Cập nhập products
+application.put('/products/:id', (req, res) => {
+    // kiểm tra lại id có tồn tại hay không
     const { id } = req.params;
-
-    // Kiểm tra user với param id có tồn tại không
-    const user = users.find(user => user.id == id);
-
-    // Nếu không tồn tại thì trả về lỗi
-    if (!user) {
-        res.status(404)
-            .send({
-                error: 'User not found'
-            });
+    const product = products.find(product => product.id == id)
+    if (!product) {
+        res.status(404).send({
+            error: "Sản phẩm không tồn tại "
+        })
     }
-
-    // Lấy request body
-    const requestBody = req.body;
-    let updatedUser = null;
-
-    users = users.map(user => {
-        if (user.id == id) {
-            updatedUser = {
-                ...user,
-                first_name: requestBody.first_name,
-                last_name: requestBody.last_name,
-                password: requestBody.password ? requestBody.password : user.password,
-                role: requestBody.role,
+    // lấy resquest body
+    const requestBody = req.body
+    let updateProduct = null;
+    products = products.map(product => {
+        if (product.id == id) {
+            updateProduct = {
+                ...product,
+                name: requestBody.name,
+                price: requestBody.price,
+                color: requestBody.color,
                 updated_at: new Date()
             }
-            return updatedUser;
+            return updateProduct
         } else {
-            return user;
+            return product
         }
-    });
 
-    // Lấy lại user sau khi update
-    // const userUpdated = users.find(user => user.id == id);
-    res.send(updatedUser)
-});
+    })
 
-application.delete('/users/:id', (req, res) => {
+    res.send(updateProduct)
+
+
+})
+// Delete products
+application.delete('/products/:id', (req, res) => {
     const { id } = req.params;
-
-    // Kiểm tra user với param id có tồn tại không
-    const user = users.find(user => user.id == id);
-
-    // Nếu không tồn tại thì trả về lỗi
-    if (!user) {
-        res.status(404)
-            .send({
-                error: 'User not found'
-            });
+    const product = products.find(product => product.id == id)
+    if (!product) {
+        res.status(404).send({
+            error: "Sản phẩm không tồn tại "
+        })
     }
-
-    users = users.filter(user => user.id != id);
+    products = products.filter(product => product.id != id)
     res.status(204).send(); // HTTP status code 204 (No Content) - thường được sử dụng để trả về sau khi xóa thành công
-});
+})
+
+
+
+// application.put('/products/:id', (req, res) => {
+//     const { id } = req.params;
+
+//     // Kiểm tra user với param id có tồn tại không
+//     const product = products.find(user => user.id == id);
+
+//     // Nếu không tồn tại thì trả về lỗi
+//     if (!product) {
+//         res.status(404)
+//             .send({
+//                 error: 'User not found'
+//             });
+//     }
+
+//     // Lấy request body
+//     const requestBody = req.body;
+//     let updatedProduct = null;
+
+//     users = users.map(user => {
+//         if (user.id == id) {
+//             updatedProduct = {
+//                 ...user,
+//                 first_name: requestBody.first_name,
+//                 last_name: requestBody.last_name,
+//                 password: requestBody.password ? requestBody.password : user.password,
+//                 role: requestBody.role,
+//                 updated_at: new Date()
+//             }
+//             return updatedUser;
+//         } else {
+//             return user;
+//         }
+//     });
+
+//     // Lấy lại user sau khi update
+//     // const userUpdated = users.find(user => user.id == id);
+//     res.send(updatedUser)
+// });
+
+// application.delete('/products/:id', (req, res) => {
+//     const { id } = req.params;
+
+//     // Kiểm tra user với param id có tồn tại không
+//     const user = users.find(user => user.id == id);
+
+//     // Nếu không tồn tại thì trả về lỗi
+//     if (!user) {
+//         res.status(404)
+//             .send({
+//                 error: 'User not found'
+//             });
+//     }
+
+//     users = users.filter(user => user.id != id);
+//     res.status(204).send(); // HTTP status code 204 (No Content) - thường được sử dụng để trả về sau khi xóa thành công
+// });
 
 application.listen(8000, () => {
     console.log('Server started');
